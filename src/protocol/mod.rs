c9c1a39803d30afae791f845121b95140bacb8e0
kayibal <alan@datarevenue.com>
2024-10-23 13:57:35 +0100

    feat: Feature gate evm dependencies
    Took 1 hour 24 minutes

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -2,14 +2,21 @@
 
 use ethers::types::{H160, H256, U256};
 use tycho_core::Bytes;
+#[cfg(feature = "evm")]
 pub mod dodo;
 pub mod errors;
 pub mod events;
 pub mod models;
 pub mod state;
+// TODO: This gate should ideally not be required
+#[cfg(feature = "evm")]
 pub mod tycho;
+#[cfg(feature = "evm")]
 pub mod uniswap_v2;
+#[cfg(feature = "evm")]
 pub mod uniswap_v3;
+// TODO: This gate should ideally not be required
+#[cfg(feature = "evm")]
 pub mod vm;
 
 /// A trait for converting types to and from `Bytes`.
