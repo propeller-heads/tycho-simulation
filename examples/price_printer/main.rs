bb5421ddf58cb2d7ae370512d627e3ba206738d1
zizou <111426680+flopell@users.noreply.github.com>
2025-08-28 09:49:28 +0200

    feat(WIP): add euler in price_printer example

--- examples/price_printer/main.rs
+++ examples/price_printer/main.rs
@@ -16,7 +16,8 @@ use tycho_simulation::{
         protocol::{
             ekubo::state::EkuboState,
             filters::{
-                balancer_v2_pool_filter, curve_pool_filter, uniswap_v4_pool_with_hook_filter,
+                balancer_v2_pool_filter, curve_pool_filter, uniswap_v4_pool_with_euler_hook_filter,
+                uniswap_v4_pool_with_hook_filter,
             },
             uniswap_v2::state::UniswapV2State,
             uniswap_v3::state::UniswapV3State,
@@ -47,27 +48,30 @@ fn register_exchanges(
     match chain {
         Chain::Ethereum => {
             builder = builder
-                .exchange::<UniswapV2State>("uniswap_v2", tvl_filter.clone(), None)
-                .exchange::<UniswapV3State>("uniswap_v3", tvl_filter.clone(), None)
-                .exchange::<EVMPoolState<PreCachedDB>>(
-                    "vm:balancer_v2",
-                    tvl_filter.clone(),
-                    Some(balancer_v2_pool_filter),
-                )
-                .exchange::<EVMPoolState<PreCachedDB>>(
-                    "vm:curve",
-                    tvl_filter.clone(),
-                    Some(curve_pool_filter),
-                )
-                .exchange::<EkuboState>("ekubo_v2", tvl_filter.clone(), None)
+                // .exchange::<UniswapV2State>("uniswap_v2", tvl_filter.clone(), None)
+                // .exchange::<UniswapV3State>("uniswap_v3", tvl_filter.clone(), None)
+                // .exchange::<EVMPoolState<PreCachedDB>>(
+                //     "vm:balancer_v2",
+                //     tvl_filter.clone(),
+                //     Some(balancer_v2_pool_filter),
+                // )
+                // .exchange::<EVMPoolState<PreCachedDB>>(
+                //     "vm:curve",
+                //     tvl_filter.clone(),
+                //     Some(curve_pool_filter),
+                // )
+                // .exchange::<EkuboState>("ekubo_v2", tvl_filter.clone(), None)
+                // .exchange::<UniswapV4State>(
+                //     "uniswap_v4",
+                //     tvl_filter.clone(),
+                //     Some(uniswap_v4_pool_with_hook_filter),
+                // );
+                // COMING SOON!
                 .exchange::<UniswapV4State>(
-                    "uniswap_v4",
+                    "uniswap_v4_hooks",
                     tvl_filter.clone(),
-                    Some(uniswap_v4_pool_with_hook_filter),
+                    Some(uniswap_v4_pool_with_euler_hook_filter),
                 );
-            // COMING SOON!
-            // .exchange::<UniswapV4State>("uniswap_v4_hooks", tvl_filter.clone(),
-            // Some(uniswap_v4_pool_with_euler_hook_filter));
             // .exchange::<EVMPoolState<PreCachedDB>>("vm:maverick_v2", tvl_filter.clone(), None)
         }
         Chain::Base => {
