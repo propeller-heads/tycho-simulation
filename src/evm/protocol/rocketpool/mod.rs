5e3b20c5f8047cbb5f8cf2e2a5984a33c0c1c1e8
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-11-26 21:21:05 +0000

    refactor: update RocketPoolState to use U256 for deposit fee and improve fee calculations

--- src/evm/protocol/rocketpool/mod.rs
+++ src/evm/protocol/rocketpool/mod.rs
@@ -3,6 +3,7 @@ use alloy::hex;
 /// Native ETH address (zero address)
 const ETH_ADDRESS: [u8; 20] = hex!("0000000000000000000000000000000000000000");
 /// Protocol Component ID for Rocket Pool
+/// TODO - consider how we can make the component ID not hardcoded?
 const ROCKET_POOL_COMPONENT_ID: &str = "0xdd3f50f8a6cafbe9b31a427582963f465e745af8";
 
 mod decoder;
