8bfd22c51cdd15ee7e4bbc1353feb305796d2a6a
Diana Carvalho <diana@propellerheads.xyz>
2025-11-27 16:01:53 +0000

    fix(EVMPoolState): After swapping, set the spot prices correctly
    Using the inverse of the price of the trade is wrong because of the fee. The trade price already has the fee deducted in this direction, to get the correct price of the inverse direction we need use the non fee price - which we don't have access to.
    Using set_spot_prices will deliver correct results, but it will make get_amount_out a little bit slower
    
    Took 25 minutes
    
    
    Took 2 minutes

--- src/evm/protocol/vm/state.rs
+++ src/evm/protocol/vm/state.rs
@@ -638,15 +638,11 @@ where
         }
 
         // Update spot prices
-        let new_price = trade.price;
-        if new_price != 0.0f64 {
-            new_state
-                .spot_prices
-                .insert((sell_token_address, buy_token_address), new_price);
-            new_state
-                .spot_prices
-                .insert((buy_token_address, sell_token_address), 1.0f64 / new_price);
-        }
+        let tokens = HashMap::from([
+            (token_in.address.clone(), token_in.clone()),
+            (token_out.address.clone(), token_out.clone()),
+        ]);
+        let _ = new_state.set_spot_prices(&tokens);
 
         let buy_amount = trade.received_amount;
 
