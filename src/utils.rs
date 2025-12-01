756b5bb0fd12f62d0c67894402fd6e6c47e16e5c
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2025-12-01 16:38:57 -0500

    fix: Limit token loading for integration test
    Otherwise it failed with:
    
    Error:   × Failed to load tokens from Tycho RPC
      ╰─▶ Fatal error: Unable to load tokens: Failed to parse response: Error: expected value at line 1 column 1, Body: Page size must be less than or equal to 3000.

--- src/utils.rs
+++ src/utils.rs
@@ -90,7 +90,7 @@ pub async fn load_all_tokens(
                 .get(&chain)
                 .or(Some(&42))
                 .copied()),
-            None,
+            Some(3000),
             RPC_CLIENT_CONCURRENCY,
         )
         .await
