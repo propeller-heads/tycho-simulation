6896761cadeb7b7c772de5be37b76c23c259d332
Ksenia Troshchenkova <ksenia@propellerheads.xyz>
2025-12-03 15:45:13 +0100

    refactor: move lido specific logic to lido state impl

--- src/evm/protocol/lido/state.rs
+++ src/evm/protocol/lido/state.rs
@@ -70,20 +70,231 @@ pub struct LidoState {
     pub stake_limits_state: StakeLimitState, //only needed for steth
 }
 
-// impl LidoState {
-//     fn steth_swap(
-//         &self,
-//         amount_in: BigUint,
-//         total_pooled_eth: BigUint,
-//         total_shares: BigUint,
-//     ) -> BigUint {
-//         todo!()
-//     }
+impl LidoState {
+    fn steth_swap(&self, amount_in: BigUint) -> GetAmountOutResult {
+        let total_pooled_eth = self.total_pooled_eth.clone();
+        let total_shares = self.total_shares.clone();
+        let shares = amount_in.clone() * total_shares.clone() / total_pooled_eth.clone();
+        let amount_out = shares.clone() * total_pooled_eth.clone() / total_shares.clone();
+        GetAmountOutResult {
+            amount: amount_out,
+            gas: BigUint::from(DEFAULT_GAS),
+            new_state: Box::new(Self {
+                pool_type: self.pool_type.clone(),
+                total_shares: self.total_shares.clone() + shares,
+                total_pooled_eth: self.total_pooled_eth.clone() + amount_in,
+                total_wrapped_st_eth: None,
+                id: self.id.clone(),
+                native_address: self.native_address.clone(),
+                stake_limits_state: self.stake_limits_state.clone(), /* this has to
+                                                                      * be
+                                                                      * updated I think */
+            }),
+        }
+    }
 
-//     fn wsteth_swap(&self, token_in: Bytes, token_out: Bytes, amount_in: BigUint) {
-//         todo!()
-//     }
-// }
+    // amount in in eth, not in stETH
+    fn wrap_steth(&self, amount_in: BigUint) -> Result<GetAmountOutResult, SimulationError> {
+        if amount_in.is_zero() {
+            return Err(SimulationError::InvalidInput("Cannot wrap 0 stETH ".to_string(), None))
+        }
+        let amount_out = amount_in * (self.total_shares.clone() / self.total_pooled_eth.clone());
+        Ok(GetAmountOutResult {
+            amount: amount_out.clone(),
+            gas: BigUint::from(DEFAULT_GAS),
+            new_state: Box::new(Self {
+                pool_type: self.pool_type.clone(),
+                total_shares: self.total_shares.clone(),
+                total_pooled_eth: self.total_pooled_eth.clone(),
+                total_wrapped_st_eth: Some(
+                    self.total_wrapped_st_eth
+                        .clone()
+                        .unwrap() +
+                        amount_out,
+                ),
+                id: self.id.clone(),
+                native_address: self.native_address.clone(),
+                stake_limits_state: self.stake_limits_state.clone(),
+            }),
+        })
+    }
+
+    // amount in is shares
+    fn unwrap_steth(&self, amount_in: BigUint) -> Result<GetAmountOutResult, SimulationError> {
+        if amount_in.is_zero() {
+            return Err(SimulationError::InvalidInput("Cannot unwrap 0 wstETH ".to_string(), None))
+        }
+
+        let amount_out = amount_in * self.total_pooled_eth.clone() / self.total_shares.clone();
+
+        Ok(GetAmountOutResult {
+            amount: amount_out.clone(),
+            gas: BigUint::from(DEFAULT_GAS),
+            new_state: Box::new(Self {
+                pool_type: self.pool_type.clone(),
+                total_shares: self.total_shares.clone(),
+                total_pooled_eth: self.total_pooled_eth.clone(),
+                total_wrapped_st_eth: Some(
+                    self.total_wrapped_st_eth
+                        .clone()
+                        .unwrap() -
+                        amount_out,
+                ),
+                id: self.id.clone(),
+                native_address: self.native_address.clone(),
+                stake_limits_state: self.stake_limits_state.clone(),
+            }),
+        })
+    }
+
+    fn st_eth_limits(
+        &self,
+        buy_token: Bytes,
+        sell_token: Bytes,
+    ) -> Result<(BigUint, BigUint), SimulationError> {
+        if buy_token == Bytes::from(ST_ETH_ADDRESS_PROXY) && sell_token == Bytes::from(ETH_ADDRESS)
+        {
+            let limit = self.stake_limits_state.get_limit();
+            Ok((limit.clone(), limit))
+        } else if buy_token == Bytes::from(ETH_ADDRESS) &&
+            sell_token == Bytes::from(ST_ETH_ADDRESS_PROXY)
+        {
+            Ok((BigUint::zero(), BigUint::zero()))
+        } else {
+            Err(SimulationError::InvalidInput(
+                format!(
+                    "Get_limits: Invalid combination of tokens for type {:?}: {:?}, {:?}",
+                    self.pool_type, buy_token, sell_token
+                ),
+                None,
+            ))
+        }
+    }
+
+    fn wst_eth_limits(
+        &self,
+        buy_token: Bytes,
+        sell_token: Bytes,
+    ) -> Result<(BigUint, BigUint), SimulationError> {
+        if buy_token == Bytes::from(ST_ETH_ADDRESS_PROXY) &&
+            sell_token == Bytes::from(WST_ETH_ADDRESS)
+        {
+            //amount of wsteth
+            Ok((
+                self.total_wrapped_st_eth
+                    .clone()
+                    .unwrap(),
+                self.total_wrapped_st_eth
+                    .clone()
+                    .unwrap(),
+            ))
+        } else if buy_token == Bytes::from(WST_ETH_ADDRESS) &&
+            sell_token == Bytes::from(ST_ETH_ADDRESS_PROXY)
+        {
+            // total_shares - wstETH
+            let limit_for_wrapping = self.total_shares.clone() -
+                self.total_wrapped_st_eth
+                    .clone()
+                    .unwrap();
+            Ok((limit_for_wrapping.clone(), limit_for_wrapping))
+        } else {
+            Err(SimulationError::InvalidInput(
+                format!(
+                    "Invalid combination of tokens for type {:?}: {:?}, {:?}",
+                    self.pool_type, buy_token, sell_token
+                ),
+                None,
+            ))
+        }
+    }
+
+    fn st_eth_delta_transition(
+        &mut self,
+        delta: ProtocolStateDelta,
+    ) -> Result<(), TransitionError<String>> {
+        self.total_pooled_eth = BigUint::from_bytes_be(
+            delta
+                .updated_attributes
+                .get("total_pooled_eth")
+                .ok_or(TransitionError::MissingAttribute(
+                    "total_pooled_eth field is missing".to_owned(),
+                ))?,
+        );
+        self.total_shares = BigUint::from_bytes_be(
+            delta
+                .updated_attributes
+                .get("total_shares")
+                .ok_or(TransitionError::MissingAttribute(
+                    "total_shares field is missing".to_owned(),
+                ))?,
+        );
+
+        let staking_status = delta
+            .updated_attributes
+            .get("staking_status")
+            .ok_or(TransitionError::MissingAttribute(
+                "Staking_status field is missing".to_owned(),
+            ))?;
+
+        let staking_status_parsed = if let Ok(status_as_str) = std::str::from_utf8(staking_status) {
+            match status_as_str {
+                "Limited" => StakingStatus::Limited,
+                "Paused" => StakingStatus::Paused,
+                "Unlimited" => StakingStatus::Unlimited,
+                _ => {
+                    return Err(TransitionError::DecodeError(
+                        "status_as_str parsed to invalid status".to_owned(),
+                    ))
+                }
+            }
+        } else {
+            return Err(TransitionError::DecodeError("status_as_str cannot be parsed".to_owned()))
+        };
+
+        let staking_limit = delta
+            .updated_attributes
+            .get("staking_limit")
+            .ok_or(TransitionError::MissingAttribute(
+                "Staking_limit field is missing".to_owned(),
+            ))?;
+
+        self.stake_limits_state = StakeLimitState {
+            staking_status: staking_status_parsed,
+            staking_limit: BigUint::from_bytes_be(staking_limit),
+        };
+        Ok(())
+    }
+
+    fn wst_eth_delta_transition(
+        &mut self,
+        delta: ProtocolStateDelta,
+    ) -> Result<(), TransitionError<String>> {
+        self.total_pooled_eth = BigUint::from_bytes_be(
+            delta
+                .updated_attributes
+                .get("total_pooled_eth")
+                .unwrap(),
+        );
+        self.total_shares = BigUint::from_bytes_be(
+            delta
+                .updated_attributes
+                .get("total_shares")
+                .ok_or(TransitionError::MissingAttribute(
+                    "total_shares field is missing".to_owned(),
+                ))?,
+        );
+        self.total_wrapped_st_eth = Some(BigUint::from_bytes_be(
+            delta
+                .updated_attributes
+                .get("total_wstETH")
+                .ok_or(TransitionError::MissingAttribute(
+                    "total_wstETH field is missing".to_owned(),
+                ))?,
+        ));
+
+        Ok(())
+    }
+}
 
 impl ProtocolSim for LidoState {
     fn fee(&self) -> f64 {
@@ -152,26 +363,7 @@ impl ProtocolSim for LidoState {
                 if token_in.address == Bytes::from(ETH_ADDRESS) &&
                     token_out.address == Bytes::from(ST_ETH_ADDRESS_PROXY)
                 {
-                    let total_pooled_eth = self.total_pooled_eth.clone();
-                    let total_shares = self.total_shares.clone();
-                    let shares =
-                        amount_in.clone() * (total_shares.clone() / total_pooled_eth.clone());
-                    let amount_out = shares.clone() * (total_pooled_eth / total_shares);
-                    Ok(GetAmountOutResult {
-                        amount: amount_out,
-                        gas: BigUint::from(DEFAULT_GAS),
-                        new_state: Box::new(Self {
-                            pool_type: self.pool_type.clone(),
-                            total_shares: self.total_shares.clone() + shares,
-                            total_pooled_eth: self.total_pooled_eth.clone() + amount_in,
-                            total_wrapped_st_eth: None,
-                            id: self.id.clone(),
-                            native_address: self.native_address.clone(),
-                            stake_limits_state: self.stake_limits_state.clone(), /* this has to
-                                                                                  * be
-                                                                                  * updated I think */
-                        }),
-                    })
+                    Ok(self.steth_swap(amount_in))
                 } else {
                     Err(SimulationError::InvalidInput(
                         format!(
@@ -186,53 +378,11 @@ impl ProtocolSim for LidoState {
                 if token_in.address == Bytes::from(ST_ETH_ADDRESS_PROXY) &&
                     token_out.address == Bytes::from(WST_ETH_ADDRESS)
                 {
-                    let amount_out = if self.total_pooled_eth.is_zero() {
-                        amount_in
-                    } else {
-                        amount_in * (self.total_shares.clone() / self.total_pooled_eth.clone())
-                    };
-                    Ok(GetAmountOutResult {
-                        amount: amount_out.clone(),
-                        gas: BigUint::from(DEFAULT_GAS),
-                        new_state: Box::new(Self {
-                            pool_type: self.pool_type.clone(),
-                            total_shares: self.total_shares.clone(),
-                            total_pooled_eth: self.total_pooled_eth.clone(),
-                            total_wrapped_st_eth: Some(
-                                self.total_wrapped_st_eth
-                                    .clone()
-                                    .unwrap() +
-                                    amount_out,
-                            ),
-                            id: self.id.clone(),
-                            native_address: self.native_address.clone(),
-                            stake_limits_state: self.stake_limits_state.clone(),
-                        }),
-                    })
+                    self.wrap_steth(amount_in)
                 } else if token_in.address == Bytes::from(WST_ETH_ADDRESS) &&
                     token_out.address == Bytes::from(ST_ETH_ADDRESS_PROXY)
                 {
-                    let amount_out =
-                        amount_in * self.total_pooled_eth.clone() / self.total_shares.clone();
-
-                    Ok(GetAmountOutResult {
-                        amount: amount_out.clone(),
-                        gas: BigUint::from(DEFAULT_GAS),
-                        new_state: Box::new(Self {
-                            pool_type: self.pool_type.clone(),
-                            total_shares: self.total_shares.clone(),
-                            total_pooled_eth: self.total_pooled_eth.clone(),
-                            total_wrapped_st_eth: Some(
-                                self.total_wrapped_st_eth
-                                    .clone()
-                                    .unwrap() -
-                                    amount_out,
-                            ),
-                            id: self.id.clone(),
-                            native_address: self.native_address.clone(),
-                            stake_limits_state: self.stake_limits_state.clone(),
-                        }),
-                    })
+                    self.unwrap_steth(amount_in)
                 } else {
                     Err(SimulationError::InvalidInput(
                         format!(
@@ -256,58 +406,8 @@ impl ProtocolSim for LidoState {
         //   - sell token is ETH: use the StakeLimitState
         // If it's wstETH: rely on the total supply (I think)
         match self.pool_type {
-            LidoPoolType::StEth => {
-                if buy_token == Bytes::from(ST_ETH_ADDRESS_PROXY) &&
-                    sell_token == Bytes::from(ETH_ADDRESS)
-                {
-                    let limit = self.stake_limits_state.get_limit();
-                    Ok((limit.clone(), limit))
-                } else if buy_token == Bytes::from(ETH_ADDRESS) &&
-                    sell_token == Bytes::from(ST_ETH_ADDRESS_PROXY)
-                {
-                    Ok((self.total_pooled_eth.clone(), self.total_shares.clone()))
-                } else {
-                    Err(SimulationError::InvalidInput(
-                        format!(
-                            "Get_limits: Invalid combination of tokens for type {:?}: {:?}, {:?}",
-                            self.pool_type, buy_token, sell_token
-                        ),
-                        None,
-                    ))
-                }
-            }
-            LidoPoolType::WStEth => {
-                if buy_token == Bytes::from(ST_ETH_ADDRESS_PROXY) &&
-                    sell_token == Bytes::from(WST_ETH_ADDRESS)
-                {
-                    // total_shares - wstETH
-                    let limit_for_wrapping = self.total_shares.clone() -
-                        self.total_wrapped_st_eth
-                            .clone()
-                            .unwrap();
-                    Ok((limit_for_wrapping.clone(), limit_for_wrapping))
-                } else if buy_token == Bytes::from(WST_ETH_ADDRESS) &&
-                    sell_token == Bytes::from(ST_ETH_ADDRESS_PROXY)
-                {
-                    //amount of wsteth
-                    Ok((
-                        self.total_wrapped_st_eth
-                            .clone()
-                            .unwrap(),
-                        self.total_wrapped_st_eth
-                            .clone()
-                            .unwrap(),
-                    ))
-                } else {
-                    Err(SimulationError::InvalidInput(
-                        format!(
-                            "Invalid combination of tokens for type {:?}: {:?}, {:?}",
-                            self.pool_type, buy_token, sell_token
-                        ),
-                        None,
-                    ))
-                }
-            }
+            LidoPoolType::StEth => self.st_eth_limits(buy_token, sell_token),
+            LidoPoolType::WStEth => self.wst_eth_limits(buy_token, sell_token),
         }
     }
 
@@ -318,77 +418,15 @@ impl ProtocolSim for LidoState {
         _balances: &Balances,
     ) -> Result<(), TransitionError<String>> {
         if delta.component_id == ST_ETH_ADDRESS_PROXY {
-            self.total_pooled_eth = BigUint::from_bytes_be(
-                delta
-                    .updated_attributes
-                    .get("total_pooled_eth")
-                    .unwrap(),
-            );
-            self.total_shares = BigUint::from_bytes_be(
-                delta
-                    .updated_attributes
-                    .get("total_shares")
-                    .unwrap(),
-            );
-
-            let staking_status = delta
-                .updated_attributes
-                .get("staking_status")
-                .ok_or(TransitionError::MissingAttribute(
-                    "Staking_status field is missing".to_owned(),
-                ))?;
-
-            let staking_status_parsed =
-                if let Ok(status_as_str) = std::str::from_utf8(staking_status) {
-                    match status_as_str {
-                        "Limited" => StakingStatus::Limited,
-                        "Paused" => StakingStatus::Paused,
-                        "Unlimited" => StakingStatus::Unlimited,
-                        _ => {
-                            return Err(TransitionError::DecodeError(
-                                "status_as_str parsed to invalid status".to_owned(),
-                            ))
-                        }
-                    }
-                } else {
-                    return Err(TransitionError::DecodeError(
-                        "status_as_str cannot be parsed".to_owned(),
-                    ))
-                };
-
-            let staking_limit = delta
-                .updated_attributes
-                .get("staking_limit")
-                .ok_or(TransitionError::MissingAttribute(
-                    "Staking_limit field is missing".to_owned(),
-                ))?;
-
-            self.stake_limits_state = StakeLimitState {
-                staking_status: staking_status_parsed,
-                staking_limit: BigUint::from_bytes_be(staking_limit),
-            };
+            self.st_eth_delta_transition(delta)
         } else if delta.component_id == WST_ETH_ADDRESS {
-            self.total_pooled_eth = BigUint::from_bytes_be(
-                delta
-                    .updated_attributes
-                    .get("total_pooled_eth")
-                    .unwrap(),
-            );
-            self.total_shares = BigUint::from_bytes_be(
-                delta
-                    .updated_attributes
-                    .get("total_shares")
-                    .unwrap(),
-            );
-            self.total_shares = BigUint::from_bytes_be(
-                delta
-                    .updated_attributes
-                    .get("total_wstETH")
-                    .unwrap(),
-            );
+            self.wst_eth_delta_transition(delta)
+        } else {
+            Err(TransitionError::DecodeError(format!(
+                "Invalid component id in delta: {:?}",
+                delta.component_id
+            )))
         }
-
-        Ok(())
     }
 
     fn clone_box(&self) -> Box<dyn ProtocolSim> {
